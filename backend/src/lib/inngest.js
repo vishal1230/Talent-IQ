7e24fdae5114a6c2bc87ceecec48b6c0d8a30224
Vishal Yadav <vishal04032003@gmail.com>
2025-12-19 09:45:31 +0530

    fixed the error in inngest.js

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import {Inngest} from "inngest";
 import { connectDB } from "./db.js";
-import {User} from "../models/index.js";
+import {User} from "../models/User.js";
 
 export const inngest = new Inngest({ id:"talent-iq"});
 
